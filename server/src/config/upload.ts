bdb2979397bfe7fc80b258d4f1b2f8de09d167d5
Edlânio Júlio <edlanioj@gmail.com>
2020-10-26 08:37:30 +0100

    ⚙ upload multer config

--- server/src/config/upload.ts
+++ server/src/config/upload.ts
@@ -2,9 +2,17 @@ import multer from 'multer';
 import path from 'path';
 import crypto from 'crypto';
 
+const storageDestination = {
+  test: path.join(__dirname, '..', '..', '__tests__', 'uploads'),
+  production: path.join(__dirname, '..', '..', 'uploads'),
+};
+
 export default {
   storage: multer.diskStorage({
-    destination: path.join(__dirname, '..', '..', 'uploads'),
+    destination:
+      storageDestination[
+        process.env.NODE_ENV === 'test' ? 'test' : 'production'
+      ],
     filename: (request, file, cb) => {
       let name = crypto.randomBytes(24).toString('hex');
       const mimetype = file.mimetype.split('/');
@@ -15,4 +23,22 @@ export default {
       cb(null, filename);
     },
   }),
-};
+  limits: {
+    fileSize: 2 * 1024 * 1024,
+    files: 5,
+  },
+  fileFilter: (req, file, cb) => {
+    const allowedMimes = [
+      'image/png',
+      'image/jpg',
+      'image/jpeg',
+      'image/pjpeg',
+    ];
+
+    if (allowedMimes.includes(file.mimetype)) {
+      cb(null, true);
+    } else {
+      cb(new Error('ficheiro não valido'));
+    }
+  },
+} as multer.Options;
