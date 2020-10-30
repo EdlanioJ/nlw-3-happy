7405373abc906eece41c6a47353d54a5269a0f5e
Edlânio Júlio <edlanioj@gmail.com>
2020-10-30 08:51:31 +0100

    feat(server): ⚙ add new routes

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -6,20 +6,50 @@ import uploadConfig from './config/upload';
 import authMiddleware from './middleware/auth';
 import authController from './controllers/authController';
 import registerController from './controllers/registerController';
+import dashboardController from './controllers/dashboardController';
+import imageController from './controllers/imageController';
 
 const routes = Router();
 const upload = multer(uploadConfig);
 
-routes.get('/orphanages', orphanageController.index);
-routes.get('/orphanages/:id', orphanageController.show);
-routes.post(
-  '/orphanages',
-  authMiddleware,
-  upload.array('images'),
-  orphanageController.create
-);
-
 routes.post('/auth', authController.create);
 routes.post('/register', registerController.create);
+
+routes.get('/orphanages', orphanageController.index);
+routes.get('/orphanages/:id', orphanageController.show);
+
+routes.get('/dashboard/orphanages', authMiddleware, dashboardController.index);
+
+routes.post(
+  '/dashboard/orphanages/image',
+  authMiddleware,
+  upload.single('image'),
+  imageController.create
+);
+
+routes.post(
+  '/dashboard/orphanages',
+  authMiddleware,
+  upload.array('images'),
+  dashboardController.create
+);
+
+routes.delete(
+  '/dashboard/orphanages/:orphanageId',
+  authMiddleware,
+  dashboardController.delete
+);
+
+routes.delete(
+  '/dashboard/orphanages/image/:orphanageId/:imageId',
+  authMiddleware,
+  imageController.delete
+);
+
+routes.put(
+  '/dashboard/orphanages/:orphanageId',
+  authMiddleware,
+  dashboardController.update
+);
 
 export default routes;
